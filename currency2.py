0923ed6c205b06bf422cce6a4365eb4439fa7290
Gaurav <s.gaurav1200@gmail.com>
2020-07-14 22:49:13 +0530

    Cross Conversion possible

--- currency2.py
+++ currency2.py
@@ -1,13 +1,16 @@
 import requests
 
 def main():
+    amount=int(input("Amount "))
+    source =input("Currency to be converted ")
     other=input("EUR to be Converted to ")
     res = requests.get("http://data.fixer.io/api/latest?access_key=a3b3fc9004011722073fb44f2423df62")
     if res.status_code!=200:
         raise Exception("ERROR IN CONNECTION WITH API")
     data=res.json()
-    rate=data["rates"][other]
-    print(f"1 EUR  is equal to{rate} {other}")
+    rate1=data["rates"][source]
+    rate2=data["rates"][other]
+    print(f"{amount} {source}  is equal to  {rate2*amount/rate1} {other}")
 
-if __name__=="__main__":
+if __name__=="__main__": 
     main()
